4b7a22d78c12f7acabf616f6228be5d1fdbd3cb6
Christoph Witzko <github@christophwitzko.com>
2021-06-14 19:03:21 +0200

    feat: add gps sensor

--- cmd/edge-service/main.go
+++ cmd/edge-service/main.go
@@ -12,6 +12,7 @@ import (
 	"github.com/fcproto/prototype/pkg/client/collector"
 	"github.com/fcproto/prototype/pkg/logger"
 	"github.com/fcproto/prototype/pkg/sensor/compass"
+	"github.com/fcproto/prototype/pkg/sensor/gps"
 	"github.com/fcproto/prototype/pkg/sensor/speed"
 	"github.com/fcproto/prototype/pkg/sensor/temperature"
 )
@@ -19,18 +20,19 @@ import (
 func main() {
 	log := logger.New("main")
 	log.Info("starting edge service...")
-	service := client.NewService("https://eng56b2lq2nnk.x.pipedream.net/", 120)
+	service := client.NewService("https://envwzzmqa85j.x.pipedream.net/", 120)
 
 	c := collector.New()
 	log.Info("registering sensors...")
 	c.RegisterSensor("speed", speed.NewSensor())
+	c.RegisterSensor("gps", gps.NewSensor())
+	c.RegisterSensor("compass", compass.NewSensor())
 	c.RegisterSensor("temperature/env", temperature.New(25, 2), collector.AggregateValues{
 		"temperature": aggregator.TypeAvg,
 	})
 	c.RegisterSensor("temperature/track", temperature.New(30, 3), collector.AggregateValues{
 		"temperature": aggregator.TypeAvg,
 	})
-	c.RegisterSensor("compass", compass.NewSensor())
 
 	log.Info("starting collector...")
 	go c.Start()
